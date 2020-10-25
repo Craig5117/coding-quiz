73ff66f64c44a4faaa5b285632388d90eda3b6c2
Craig5117 <craig.bennett5117@gmail.com>
2020-10-25 00:23:29 -0400

    stuck trying to add more questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,18 +5,25 @@ var quizArea = document.querySelector("main");
 var startBtn = document.querySelector("#start-quiz");
 var buttonContainer = document.querySelector(".button-container");
 var result = document.querySelector("#result")
+var resultsArea = document.querySelector("#results-area")
 var evaluate = "";
+var answer = ""
 
 var questions = [
-    { q: 'The sky is blue.', a: 'fork'}
-    // { q: 'There are 365 days in a year.', a: 't' },
-    // { q: 'There are 42 ounces in a pound.', a: 'f' },
-    // { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    // { q: 'Bananas are vegetables.', a: 'f' }
+    { q: 'The sky is blue.', a: 'fork'},
+    // { q: 'There are 365 days in a year.', a: 'house' },
+    // { q: 'There are 42 ounces in a pound.', a: 'beer' },
+    // { q: 'The Declaration of Independence was created in 1745.', a: 'snake' },
+    // { q: 'Bananas are vegetables.', a: 'spoon' }
   ];
 
 var answers = [
-    {m1:'fork', m2:'spoon', m3:'pickle', m4:'hexagon'}
+    {m1:'fork', m2:'spoon', m3:'pickle', m4:'hexagon'},
+    // {m1:'mouse', m2:'house', m3:'louse', m4:'laos'},
+    // {m1:'beer', m2:'fear', m3:'year', m4:'tear'},
+    // {m1:'monkey', m2:'cat', m3:'horse', m4:'snake'},
+    // {m1:'fork', m2:'spoon', m3:'pickle', m4:'hexagon'}
+
 ];
 
 var timeCount = function(){
@@ -40,7 +47,7 @@ var timeCount = function(){
 var resultDisplay = function() {
     var timeLeft = 1;
     result.textContent = evaluate
-    result.style.borderTop = "3px solid lightslategray";
+    resultsArea.style.borderTop = "3px solid lightslategray";
     var timeInterval = setInterval(function() {
         
         if (timeLeft > 0){
@@ -50,7 +57,7 @@ var resultDisplay = function() {
         else if (timeLeft === 0) {
             clearInterval(timeInterval);
             result.textContent = "";
-            result.style.borderTop = "none";
+            resultsArea.style.borderTop = "none";
         }
     }, 1000);
 }
@@ -59,12 +66,13 @@ var resultDisplay = function() {
 // initialize quiz function
 var quiz = function(){
     timeCount();
+    
     quizArea.style.justifyContent = "flex-start";
     quizArea.style.alignItems = "flex-start";
     for (var i = 0; i < questions.length; ++i) {
     // create qs
     question.textContent = questions[i].q
-    var answer = questions[i].a;
+    answer = questions[i].a;
     // create multiple choice
     
     var button1 = document.createElement("button");
@@ -88,20 +96,24 @@ var quiz = function(){
     buttonContainer.appendChild(button3);
     buttonContainer.appendChild(button4);
     
-    // compares user input to answer
-    var submitAndCompare = function(){
-        var answerSubmission = event.target.textContent
-        
-        if (answerSubmission === answer){
-            evaluate = "Correct!";
-        }
-        else {
-            evaluate = "Wrong!";
-        }
-        resultDisplay();
-        
+        // compares user input to answer
+        var submitAndCompare = function(){
+            var answerSubmission = event.target.textContent
+            button1.remove();
+            button2.remove();
+            button3.remove();
+            button4.remove();
+            
+            if (answerSubmission === answer){
+                evaluate = "Correct!";
+            }
+            else {
+                evaluate = "Wrong!";
+            }
+            resultDisplay();
+            
 
-    }
+        }
 
     button1.addEventListener("click", submitAndCompare); 
     button2.addEventListener("click", submitAndCompare); 

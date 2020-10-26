bb2fc116c6d7ff22d922d133896e5effeaf76255
Craig5117 <craig.bennett5117@gmail.com>
2020-10-26 01:00:14 -0400

    still kind of buggy

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,21 +1,32 @@
+var goBack = document.querySelector("#go-back")
+var clearScores = document.querySelector("#clear-scores") 
+
 var loadScores = function(){
     var highScores = JSON.parse(localStorage.getItem("highScores"));
     console.log (highScores);
     var scoreTable = document.querySelector("#score-table")
     if (highScores){
         for (var i = 0; i < highScores.length; i++){
-            var scoreInfo = highScores[i];
+            
             var trEl = document.createElement("tr")
             var nameTdEl = document.createElement("td")
             var scoreTdEl = document.createElement("td")
         
-            nameTdEl.textContent = scoreInfo.name
-            scoreTdEl.textContent = scoreInfo.score
+            nameTdEl.textContent = highScores[i].name
+            scoreTdEl.textContent = highScores[i].score
         
             trEl.append(nameTdEl, scoreTdEl)
             scoreTable.append(trEl)
+            console.log(trEl);
         }
     }
 }
 
 loadScores();
+
+var reloadMain = function() { 
+    return window.location.assign((href = "./index.html"))
+}
+
+
+goBack.addEventListener("click", reloadMain);
